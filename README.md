071bf29d6615741417cf3693db89a08a0da10621
Axel Gard <axel.gard@protonmail.com>
2025-09-19 15:33:11 +0200

    added fast IO

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # 1brc
 he One Billion Row Challenge
+
+
+data from https://github.com/gunnarmorling/1brc/blob/main/data/weather_stations.csv
+
+## build
+
+```bash
+make
+time ./main ./weather_stations.csv
+```
+
+## performance
+
+My time on my macbook pro 14 m3 pro
+
+### simple solution
+```
+-54.9333/25.9337/81.7166
+
+real    0m0.054s
+user    0m0.044s
+sys     0m0.007s
+```
--- src/main.cpp
+++ src/main.cpp
@@ -8,55 +8,73 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(std::string s, char delimiter) {
-  std::vector<std::string> tokens;
-  std::string token = "";
-  for (char c : s) {
-    if (c == delimiter) {
-      tokens.push_back(token);
-      token = "";
-    } else {
-      token += c;
-    }
-  }
-  tokens.push_back(token);
+#define FASTIO                   \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);           \
+    std::cout.precision(11)
 
-  return tokens;
+std::vector<std::string> split(std::string s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token = "";
+    for (char c : s)
+    {
+        if (c == delimiter)
+        {
+            tokens.push_back(token);
+            token = "";
+        }
+        else
+        {
+            token += c;
+        }
+    }
+    tokens.push_back(token);
+
+    return tokens;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    FASTIO;
+    assert(argc == 2);
 
-  assert(argc == 2);
-  char *FILENAME = argv[1];
+    char *FILENAME = argv[1];
 
-  FILE *fp = fopen(FILENAME, "r");
-  if (fp == NULL)
-    exit(EXIT_FAILURE);
+    FILE *fp = fopen(FILENAME, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
 
-  char *line = NULL;
-  size_t len = 0;
-  float min = std::numeric_limits<float>::max();
-  float max = std::numeric_limits<float>::min();
-  float mean = 0.0;
-  int row_count = 0;
-  while ((getline(&line, &len, fp)) != -1) {
-    auto v = split(std::string(line), ';');
-    assert(v.size() == 2);
-    std::string place = v[0];
-    float deg = std::stof(v[1]);
-    if (deg > max) {
-      max = deg;
+    char *line = NULL;
+    size_t len = 0;
+    float min = std::numeric_limits<float>::max();
+    float max = std::numeric_limits<float>::min();
+    float mean = 0.0;
+    int row_count = 0;
+
+    while ((getline(&line, &len, fp)) != -1)
+    {
+        auto v = split(std::string(line), ';');
+        assert(v.size() == 2);
+        std::string place = v[0];
+        float deg = std::stof(v[1]);
+        if (deg > max)
+        {
+            max = deg;
+        }
+        if (deg < min)
+        {
+            min = deg;
+        }
+        mean += deg;
+        row_count++;
     }
-    if (deg < min) {
-      min = deg;
-    }
-    mean += deg; 
-    row_count++;
-  }
-  fclose(fp);
-  if (line)
-    free(line);
 
-  std::cout << min << "/" << mean / row_count << "/" << max << std::endl;
-  return 0;
+    fclose(fp);
+    if (line)
+        free(line);
+
+    std::cout << min << "/" << mean / row_count << "/" << max << std::endl;
+    return 0;
 }
